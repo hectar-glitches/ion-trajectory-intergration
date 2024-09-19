0920a7ed5b0813d7718d27a10a2c756c52d8e98b
hectar-glitches <hectar@uni.minerva.edu>
2024-09-19 15:44:05 +0800

    commit thursday

--- program.py
+++ program.py
@@ -3,66 +3,75 @@ import math
 pygame.init()
 
 WHITE = (255, 255, 255)
+BLACK = (0,0,0)
 BLUE = (176,224,230)
 
 WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+"""
+magnetic field strength = B
+electric field strength = E
+initial gyrophase = theta_0
+drift velocity = U
+"""
+
+E = 50
+B = 10
+theta_0 = 0
+
+
 pygame.display.set_caption("Ion Trajectory")
 
 class Ion:
-    def __init__(self, x, y, init_velocity, charge, mass, time, color):
+    def __init__(self, x, y, charge, mass, time, color):
         self.x = x
         self.y = y
-        self.init_velocity = init_velocity
         self.charge = charge
         self.mass = mass
         self.time = time
         self.color = color
-        self.motion = []
 
-    def draw(self, win):
-        pygame.draw.arc(win, self.color, (WIDTH/2 - radius, HEIGHT/2 - radius, 2*radius, 2*radius), 0, math.radians(90), 2)
+    def draw(self, WIN):
+        points = self.trajectory()
+        pygame.draw.lines(WIN, self.color, False, points, 2)
 
 
 
     def trajectory(self):
-        """
-        magnetic field strength = B
-        electric field strength = E
-        initial gyrophase = theta_0
-        drift velocity = U
-        """
-        E = 50
-        B = 10
-        theta_0 = 0
-        time = 0
+        self.motion = []
+        self.t = 0
+
+        dt = 0.01
 
         angular_position = self.charge * B / self.mass
 
         U = E * B / B**2
         gyroradius = (self.mass * U) / self.charge * E
 
-        motion = []
+        while self.t < self.time:
+            x = gyroradius * ((angular_position * self.t + theta_0) - math.sin(angular_position * self.t + theta_0))
+            y = gyroradius * (1 - math.cos(angular_position * self.t + theta_0))
 
-        while time != self.time:
-            self.x = gyroradius * ((angular_position * time + theta_0) - math.sin(angular_position * time + theta_0))
-            self.y = gyroradius * (1 - math.cos(angular_position * time + theta_0))
-            time += 1
+            x += WIDTH//2
+            y += HEIGHT//2
 
-            motion.append((self.x, self.y))
+            self.motion.append((x, y))
+
+            self.t += dt
 
 
-
+        
+        return self.motion
 
 
 def main():
     run = True
     clock = pygame.time.Clock()
 
-    hydrogen  = Ion(0, 0, 10, +1, 1, 5, WHITE)
-    helium = Ion(0, 0, 5, +2, +2, 5, BLUE)
+    hydrogen  = Ion(0, 0, +6, 1, 5, BLACK)
+    helium = Ion(0, 0, +2, +2, 5, BLUE)
 
     ions = [hydrogen, helium]
 
@@ -80,6 +89,6 @@ def main():
 
         pygame.display.update()
 
-    pygame.QUIT()
+    pygame.quit()
 
 main()
